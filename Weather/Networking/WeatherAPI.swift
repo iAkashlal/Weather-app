1b36a1aa352c1ebe9ea01e3eb4da43ee62056ca9
AkashlalBol <akashlal.bathe@lji.io>
2022-03-12 23:30:53 +0530

    Implemented search City API call with Moya

--- Weather/Networking/WeatherAPI.swift
+++ Weather/Networking/WeatherAPI.swift
@@ -8,6 +8,61 @@
 import Foundation
 import Moya
 
-class WeatherAPI {
+struct OpenWeather {
+    static let baseURL = "https://api.openweathermap.org/"
+    static let apiKey = "3a5fabe12f5bccdfca69c983db89f6bb"
+}
+
+enum WeatherAPI {
+    static let provider = MoyaProvider<WeatherAPI> (
+        plugins: [
+            NetworkLoggerPlugin (
+                configuration: .init(logOptions: .verbose)
+            )
+        ]
+    )
+    
+    case seachText(text: String)
+}
+
+extension WeatherAPI: TargetType {
+    
+    var baseURL: URL {
+        URL(string: OpenWeather.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .seachText:
+            return "geo/1.0/direct"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .seachText:
+            return .get
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case let .seachText(locationText):
+            let payload = [
+                "q": locationText.replacingOccurrences(of: " ", with: ""),
+                "limit": "15",
+                "appid": OpenWeather.apiKey
+            ]
+            return .requestParameters(
+                parameters: payload,
+                encoding: JSONEncoding.default
+            )
+        }
+    }
+    
+    var headers: [String : String]? {
+        ["Content-Type": "application/json"]
+    }
+    
     
 }
