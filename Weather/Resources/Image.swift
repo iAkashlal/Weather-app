eddcb09fd99d212c12ac4c4f045b2ee919420290
AkashlalBol <akashlal.bathe@lji.io>
2022-03-13 16:06:52 +0530

    Added and linked empty state views

---
+++ Weather/Resources/Image.swift
@@ -0,0 +1,21 @@
+//
+//  Image.swift
+//  Weather
+//
+//  Created by akashial on 13/03/22.
+//
+
+import UIKit
+
+// ENUM returning images stored in Assets.xcassets images
+enum Image {
+    
+    static var caratArrow: UIImage {
+        return UIImage.init(named: "carat")!
+    }
+    
+    static var emptyResults: UIImage {
+        return UIImage.init(named: "emptyResult")!
+    }
+}
+
